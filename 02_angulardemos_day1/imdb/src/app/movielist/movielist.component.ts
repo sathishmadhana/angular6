f92752d84dcc8d705742c75b65e21ed6d7661930
Sathish <sathishmadhana@gmail.com>
2019-07-02 15:36:57 +0530

    feat: create a service and move api call to it

--- 02_angulardemos_day1/imdb/src/app/movielist/movielist.component.ts
+++ 02_angulardemos_day1/imdb/src/app/movielist/movielist.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
+import { MovielistService } from '../movielist.service';
 
 @Component({
   selector: 'app-movielist',
@@ -10,13 +11,12 @@ export class MovielistComponent implements OnInit {
 
   movies;
 
-  constructor(public httpClient: HttpClient) { }
+  constructor(public service: MovielistService) { }
 
-  ngOnInit() {  // Will get called as soon as the UI is loaded
-    this.httpClient.get("http://www.mocky.io/v2/5d1b11233400004c000004ae?mocky-delay=3000ms")
-    .subscribe(
-      result => this.movies = result
-    );
+  ngOnInit() {  // Will get called as soon as the UI is loaded        
+    this.service.getMovieList().subscribe(result => {
+      this.movies = result
+    });
   }
 
   handleClick(evt) {
