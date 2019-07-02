1dc790aac8d995fae73168ba770fcbb284e0d276
Sathish <sathishmadhana@gmail.com>
2019-07-02 22:50:41 +0530

    feat: add array filtering to display corresponding page details

--- 02_angulardemos_day1/imdb/src/app/movielist/movielist.component.ts
+++ 02_angulardemos_day1/imdb/src/app/movielist/movielist.component.ts
@@ -18,6 +18,16 @@ export class MovielistComponent implements OnInit {
       name : "Harry Potter", 
       summary: 'Harry Potter is a British-American film series based on the eponymous novels by author J. K. Rowling. The series is distributed by Warner Bros. and consists of eight fantasy films', 
       banner: 'http://t0.gstatic.com/images?q=tbn:ANd9GcT_9nrOnN8sYfZZHJ06EIBSoEO5qjx7uHHEs6VtKNplGVuGhZuC'
+    },
+    {
+      name : "James Bond", 
+      summary: 'The James Bond series focuses on a fictional British Secret Service agent created in 1953 by writer Ian Fleming, who featured him in twelve novels and two short-story collections. Since Flemings death in 1964, eight other authors have written authorised Bond novels', 
+      banner: 'https://i.pinimg.com/originals/12/16/fd/1216fd4c59380dd8f5c2f3b0e8b4631d.jpg'
+    },
+    {
+      name : "Sherlock Holmes", 
+      summary: 'Sherlock Holmes is a 2009 mystery period action film based on the character of the same name created by Sir Arthur Conan Doyle. The film was directed by Guy Ritchie and produced by Joel Silver, Lionel Wigram, Susan Downey, and Dan Lin. The screenplay, by Michael Robert Johnson, Anthony Peckham, and Simon Kinberg, was developed from a story by Wigram and Johnson', 
+      banner: 'https://m.media-amazon.com/images/M/MV5BMTg0NjEwNjUxM15BMl5BanBnXkFtZTcwMzk0MjQ5Mg@@._V1_.jpg'
     }
   ];
 
@@ -33,9 +43,11 @@ export class MovielistComponent implements OnInit {
 
   handleClick(evt) {    
     
-    //var movieName = evt.target.innerHTML;
+    var movieName = evt.target.innerHTML;
     //var movieDetail = this.moviedetails.filter(result => result.name === movieName);
-    this.pubsubservice.getPublisher().next(this.moviedetails[0]);
+
+    var movieDetail = this.moviedetails.find(item => item.name === movieName);
+    this.pubsubservice.getPublisher().next(movieDetail);
     
   }
 
