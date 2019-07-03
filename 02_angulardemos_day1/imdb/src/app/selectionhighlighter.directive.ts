8d243f2ebd94dfed9644a07c0d04888d64e92593
Sathish <sathishmadhana@gmail.com>
2019-07-03 17:54:59 +0530

    feat: add input params to directives

--- 02_angulardemos_day1/imdb/src/app/selectionhighlighter.directive.ts
+++ 02_angulardemos_day1/imdb/src/app/selectionhighlighter.directive.ts
@@ -1,20 +1,25 @@
 import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[appSelectionhighlighter]'  // for attribute
+  selector: '[appSelectionhighlighter]',  // for attribute
   // selector: 'appSelectionhighlighter' // for usage as element (tag)
   // selector: '.appSelectionhighlighter' // for usage as class
+  inputs: ['oncolor', 'offcolor']
 })
 export class SelectionhighlighterDirective {
+  oncolor: string;
+  offcolor: string;
 
   constructor(elem: ElementRef) {
     elem.nativeElement.style = 'color:red';
-    //elem.nativeElement.innerHTML = '<div class="jumbotron">Hello</div>';
+    //elem.nativeElement.innerHTML = '<div class="jumbotron">Hello</div>';    
   }
 
   @HostListener("mouseover")
   whenmousover(){
     console.log("mouse over");
+    console.log('oncolor : '+this.oncolor);
+    console.log('offcolor : '+this.offcolor);
   }
 
   @HostListener("mouseout")
