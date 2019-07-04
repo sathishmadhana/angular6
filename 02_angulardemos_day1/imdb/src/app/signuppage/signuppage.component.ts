b93dc5304f86b65725ac4d3968eaf431d3b66434
Sathish <sathishmadhana@gmail.com>
2019-07-04 09:35:01 +0530

    feat: loacal storage implementation but incomplete, needs to be implemented

--- 02_angulardemos_day1/imdb/src/app/signuppage/signuppage.component.ts
+++ 02_angulardemos_day1/imdb/src/app/signuppage/signuppage.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignuppageComponent implements OnInit {
 
+  cities = [];
   constructor() { }
 
   ngOnInit() {
+    let localcities = localStorage.getItem("cities");
+    if(!localcities){
+      //localStorage.setItem("cities", atob(mypayload))  //TODO: local storage example, implement later
+    } else {
+      //this.cities = localcities;
+    }
   }
 
 }
